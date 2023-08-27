8f2805bbbe6442584051e181a9b657663ea4b4aa
Elias Rissanen Uran <elura001@osloskolen.no>
2023-08-27 16:58:47 +0200

    xdd

--- code/src/components/Gallery components/fetchRats.tsx
+++ code/src/components/Gallery components/fetchRats.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 interface Props {
   id: string;
-  onRatsLoaded: () => void;
+  // onRatsLoaded: () => void;
 }
 
 interface innerItem {
@@ -17,39 +17,43 @@ interface RatData {
   [key: string]: innerItem[];
 }
 
-const FetchRats = ({ id, onRatsLoaded }: Props) => {
+const FetchRats = ({ id }: Props) => {
   const selectedArray = data.find((item: RatData) => id in item) as RatData;
 
-  useEffect(() => {
-    if (selectedArray) {
-      onRatsLoaded();
-    }
-  }, [selectedArray, onRatsLoaded]);
+  if (!selectedArray) {
+    return (
+      <div className="text-text text-9xl">
+        No rats with this id blud. only use links from the website if u wanna
+        see rats
+      </div>
+    );
+  }
+
+  const items = selectedArray[id];
+
+  // useEffect(() => {
+  //   if (items) {
+  //     onRatsLoaded();
+  //   }
+  // }, [items]);
 
   return (
     <div className="h-full flex flex-wrap justify-center items-center gap-12 p-8">
-      {selectedArray ? (
-        selectedArray[id].map((innerItem: innerItem, innerIndex: number) => (
-          <div
-            key={innerIndex}
-            className="flex flex-col items-center gap-8 break-all text-center"
-          >
-            <Link href={innerItem.link} target="_blank">
-              <img
-                src={innerItem.img}
-                alt={innerItem.name}
-                className="border-2 border-accent rounded-md h-48 max-w-sm"
-              />
-            </Link>
-            <h3 className="text-text text-6xl font-Berlin">{innerItem.name}</h3>
-          </div>
-        ))
-      ) : (
-        <div className="text-text text-9xl">
-          No rats with this id blud. only use links from the website if u wanna
-          see rats
+      {items.map((innerItem: innerItem, innerIndex: number) => (
+        <div
+          key={innerIndex}
+          className="flex flex-col items-center gap-8 break-all text-center"
+        >
+          <Link href={innerItem.link} target="_blank">
+            <img
+              src={innerItem.img}
+              alt={innerItem.name}
+              className="border-2 border-accent rounded-md h-48 max-w-sm"
+            />
+          </Link>
+          <h3 className="text-text text-6xl font-Berlin">{innerItem.name}</h3>
         </div>
-      )}
+      ))}
     </div>
   );
 };
--- code/src/pages/gallery/[id].tsx
+++ code/src/pages/gallery/[id].tsx
@@ -17,34 +17,34 @@ const SubGalleryBody = () => {
     }
   }, [url]);
 
-  const [ratsNotReady, setRatsNotReady] = useState(true);
+  // const [ratsNotReady, setRatsNotReady] = useState(true);
 
-  useEffect(() => {
-    setRatsNotReady(true);
-  }, [id]);
+  // useEffect(() => {
+  //   setRatsNotReady(true);
+  // }, [id]);
 
-  const handleRatsLoaded = () => {
-    setRatsNotReady(false);
-  };
+  // const handleRatsLoaded = () => {
+  //   setRatsNotReady(false);
+  // };
 
-  const onRatsLoadedCallback = useCallback(handleRatsLoaded, []);
+  // const onRatsLoadedCallback = useCallback(handleRatsLoaded, []);
 
-  useEffect(() => {
-    if (ratsNotReady) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-  }, [ratsNotReady]);
+  // useEffect(() => {
+  //   if (ratsNotReady) {
+  //     document.body.style.overflow = "hidden";
+  //   } else {
+  //     document.body.style.overflow = "unset";
+  //   }
+  // }, [ratsNotReady]);
 
   return (
     <main className="bg-bg min-h-screen p-16">
-      {ratsNotReady && <Loading />}
+      {/* {ratsNotReady && <Loading />} */}
       <div className="text-text font-Berlin text-8xl uppercase">
         <span className="text-lg">gallery/</span>
         <h1>{id}</h1>
       </div>
-      {id && <FetchRats id={id} onRatsLoaded={onRatsLoadedCallback} />}
+      {id && <FetchRats id={id} />}
     </main>
   );
 };
